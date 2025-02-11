80b8e99d2dc7925042a6fa145bf7a35cdbba79ca
ABHIJEET VISHWAKARMA <abhijeetdrv@gmail.com>
2025-02-12 00:52:50 +0530

    card height fixed

--- components/home/benefits.tsx
+++ components/home/benefits.tsx
@@ -87,7 +87,7 @@ export function Benefits() {
               key={index}
               id={`q3qpvb_${index}`}
             >
-              <Card className="border-none shadow-none bg-accent/50">
+              <Card className="border-none shadow-none h-full bg-accent/50">
                 <CardContent className="pt-6" id={`ijh8dq_${index}`}>
                   <div
                     className="flex flex-col items-start gap-4"
